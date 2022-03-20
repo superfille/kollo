c163e25c0510b031b9dd876078febd82f35c2015
Filip Anic <superfilles12@gmail.com>
2022-03-20 12:09:50 +0100

    Fixed some css

--- createCheckbox.js
+++ createCheckbox.js
@@ -1,6 +1,6 @@
 const createCheckbox = (obj) => {
   return `
-    <div>
+    <div class="margin-right-normal">
       <input type="checkbox" id="${obj.id}" name="${obj.name}" value="${obj.value}">
       <label for="${obj.id}">${obj.text}</label>
     </div>
